e388aad2f5b2ba30d0a3bcd118b83f45044e3740
Souza <rafaelalexsouza1337@gmail.com>
2024-11-07 17:30:43 +0000

    dist update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       <header className="flex justify-between items-center p-4 bg-red-600">
         <div className="flex items-center space-x-4">
           <img
-            src="public/dxplogo.png" 
+            src="dist\dxplogo.png" 
             alt="DriverXPress Logo" 
             className="h-12 w-12 logo-spin" 
           />
@@ -35,7 +35,7 @@ export default function App() {
           </a>
           <div className="relative">
            <a href="https://github.com/souzaisrage/dxp-driversapp" target='_blank'><button className="text-white hover:text-black transition-colors duration-300 ease-in-out flex items-center">
-              <img src="public/Orion_github.svg" 
+              <img src="dist\Orion_github.svg" 
               alt="Github Icon" 
               className="mr-2 h-6 w-6" />
             Github
@@ -47,7 +47,7 @@ export default function App() {
       <section className="relative flex items-center justify-center h-96 bg-gray-900">
         <video 
           className="absolute inset-0 w-full h-full object-cover blur-md" 
-          src="public/redloopvideo.mp4" 
+          src="dist\redloopvideo.mp4" 
           autoPlay 
           muted 
           loop
@@ -62,7 +62,7 @@ export default function App() {
         <div className="flex flex-col md:flex-row items-center md:space-x-8">
           <div className="md:w-1/2 mb-6 md:mb-0">
             <img 
-              src="public/imagedude.png" 
+              src="dist\imagedude.png" 
               className="rounded-lg shadow-lg"
             />
           </div>
