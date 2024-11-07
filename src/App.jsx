3f5bca3c33bd58a126578d25e2552b1c399ecd27
Souza <rafaelalexsouza1337@gmail.com>
2024-11-07 17:45:53 +0000

    image ghpages

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       <header className="flex justify-between items-center p-4 bg-red-600">
         <div className="flex items-center space-x-4">
           <img
-            src="dist\dxplogo.png" 
+            src="dxplogo.png" 
             alt="DriverXPress Logo" 
             className="h-12 w-12 logo-spin" 
           />
@@ -26,7 +26,7 @@ export default function App() {
             </svg>
             FAQs
           </button>
-         <a href="https://paypal.me/Souzawow?country.x=PT&locale.x=en_US" target='_blank'><button className="text-white hover:text-black transition-colors duration-300 ease-in-out flex items-center">
+         <a href="https://paypal.me/Souzawow" target='_blank'><button className="text-white hover:text-black transition-colors duration-300 ease-in-out flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="39px" fill="#e8eaed">
           <path d="M640-440 474-602q-31-30-52.5-66.5T400-748q0-55 38.5-93.5T532-880q32 0 60 13.5t48 36.5q20-23 48-36.5t60-13.5q55 0 93.5 38.5T880-748q0 43-21 79.5T807-602L640-440Zm0-112 109-107q19-19 35-40.5t16-48.5q0-22-15-37t-37-15q-14 0-26.5 5.5T700-778l-60 72-60-72q-9-11-21.5-16.5T532-800q-22 0-37 15t-15 37q0 27 16 48.5t35 40.5l109 107ZM280-220l278 76 238-74q-5-9-14.5-15.5T760-240H558q-27 0-43-2t-33-8l-93-31 22-78 81 27q17 5 40 8t68 4q0-11-6.5-21T578-354l-234-86h-64v220ZM40-80v-440h304q7 0 14 1.5t13 3.5l235 87q33 12 53.5 42t20.5 66h80q50 0 85 33t35 87v40L560-60l-280-78v58H40Zm80-80h80v-280h-80v280Zm520-546Z"/>
           </svg>
@@ -35,7 +35,7 @@ export default function App() {
           </a>
           <div className="relative">
            <a href="https://github.com/souzaisrage/dxp-driversapp" target='_blank'><button className="text-white hover:text-black transition-colors duration-300 ease-in-out flex items-center">
-              <img src="dist\Orion_github.svg" 
+              <img src="Orion_github.svg" 
               alt="Github Icon" 
               className="mr-2 h-6 w-6" />
             Github
@@ -47,7 +47,7 @@ export default function App() {
       <section className="relative flex items-center justify-center h-96 bg-gray-900">
         <video 
           className="absolute inset-0 w-full h-full object-cover blur-md" 
-          src="dist\redloopvideo.mp4" 
+          src="redloopvideo.mp4" 
           autoPlay 
           muted 
           loop
@@ -62,7 +62,7 @@ export default function App() {
         <div className="flex flex-col md:flex-row items-center md:space-x-8">
           <div className="md:w-1/2 mb-6 md:mb-0">
             <img 
-              src="dist\imagedude.png" 
+              src="imagedude.png" 
               className="rounded-lg shadow-lg"
             />
           </div>
