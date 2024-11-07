55fb8aea37a0e7928c2c2e7ab0906b6b0cd90ce0
souzaisrage <rafaelalexsouza1337@gmail.com>
2024-11-07 12:49:39 +0000

    ghpages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This ensures it's correctly served from the root in local development
+  base: '/dxp-driversite/', // This ensures it's correctly served from the root in local development
   build: {
     outDir: 'dist',
   },
